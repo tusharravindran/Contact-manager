f7fc890e9fdc75bd1565792540624b54913a85c5
tushar R <tusharrravindran@gmail.com>
2022-09-26 18:09:46 +0530

    third

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,4 +5,19 @@ class ContactsController < ApplicationController
   def new
    @contact = Contact.new
   end
+  def create 
+  @contact = Contact.new(contact_params)
+  if @contact.save
+    flash[:success] = "Successfully created new contact"
+    redirect_to contacts_path
+  else
+    render 'new'
+  end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :company, :adress ,:phone ,:dob) 
+  end
 end
