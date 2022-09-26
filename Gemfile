2077de9bafdd97d17a24277fed791d6430c86942
tushar R <tusharrravindran@gmail.com>
2022-09-26 13:51:27 +0530

    db migrate

--- Gemfile
+++ Gemfile
@@ -21,12 +21,16 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'jquery-rails'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
+#Bootstrap SAAS
+gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
+
+gem 'faker', '~> 2.23'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Contact.destroy_all
+
+
+contacts=[]
+
+20.times do |i|
+  new_contact ={
+    id: i
+    name: Faker::Name.name, 
+    email: Faker::Internet.email, 
+    company: Faker::Company.name
+    address: "#{Faker:Address.street_name} #{Faker::Address.zip} #{Faker::Address.city}"
+    phone: Faker::PhoneNumber.cell_phone
+  }
+  contacts.push(new_contact)
+end
+
+# save to DB
+Contact.create(contacts)
